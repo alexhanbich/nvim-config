3c67ece9c9df41c8e35eee44362146cd1be32ba1
Alex Kang <63813437+alexhanbich@users.noreply.github.com>
2023-05-22 15:02:43 -0600

    Update remaps.lua

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -1,10 +1,10 @@
 -- diagnostic
-vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message', silent = true })
+vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message', silent = true })
+vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message', silent = true })
+vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, { desc = 'Open diagnostics list', silent = true })
 
-vim.keymap.set('n', '<leader>w', ':w<cr>', { silent = true })
+vim.keymap.set('n', '<leader>w', ':w<cr>', { dest = 'Save', silent = true })
 
 -- highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
